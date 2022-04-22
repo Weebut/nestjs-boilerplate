14bc8060eb61863027335993f338a7a37dd08258
Seunghyuk <seunghyukoh0@gmail.com>
2022-04-22 20:07:59 +0900

    Add base domain event class

---
+++ src/libs/domain/base-classes/base-domain-event.ts
@@ -0,0 +1,30 @@
+import { Guard } from '../guard';
+import { UUID } from '../value-objects/uuid.value-object';
+
+export type DomainEventProps<T> = Omit<
+  T,
+  'id' | 'correlationId' | 'dateOccurred'
+> &
+  Omit<BaseDomainEvent, 'id' | 'correlationId' | 'dateOccurred'> & {
+    correlationId?: string;
+    dateOccurred?: number;
+  };
+
+export abstract class BaseDomainEvent {
+  public readonly id: string;
+
+  public readonly aggregateId: string;
+  public readonly dateOccurred: number;
+  public correlationId: string;
+  public causationId?: string;
+
+  constructor(props: DomainEventProps<unknown>) {
+    if (Guard.isEmpty(props)) {
+      throw new Error('DomainEvent props should not be empty');
+    }
+    this.id = UUID.generate().unpack();
+    this.aggregateId = props.aggregateId;
+    this.dateOccurred = props.dateOccurred || Date.now();
+    if (props.correlationId) this.correlationId = props.correlationId;
+  }
+}
