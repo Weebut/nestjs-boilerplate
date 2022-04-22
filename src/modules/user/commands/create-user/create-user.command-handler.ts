3b14d03d2f2c78dcbeff1943f57631e61a66a666
Seunghyuk <seunghyukoh0@gmail.com>
2022-04-22 15:36:13 +0900

    Update create user command handler for test

--- src/modules/user/commands/create-user/create-user.command-handler.ts
+++ src/modules/user/commands/create-user/create-user.command-handler.ts
@@ -1,10 +1,16 @@
 import { CommandHandler } from '@nestjs/cqrs';
 import { BaseCommandHandler } from 'src/libs/domain/base-classes/base-command-handler';
+import { User } from '../../domain/entities/user.entity';
+import { Foo } from '../../domain/value-objects/foo.value-object';
 import { CreateUserCommand } from './create-user.command';
 
 @CommandHandler(CreateUserCommand)
 export class CreateUserCommandHandler extends BaseCommandHandler {
   async handle(command: CreateUserCommand) {
-    return command;
+    const user = User.create({
+      foo: new Foo(command.foo),
+    });
+
+    return user;
   }
 }
