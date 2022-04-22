d6ac582c7dd3bd312df993e5b236773ad31c69bc
Seunghyuk <seunghyukoh0@gmail.com>
2022-04-22 15:13:21 +0900

    Use command handler to handle user create command

--- src/modules/user/commands/create-user/create-user.controller.ts
+++ src/modules/user/commands/create-user/create-user.controller.ts
@@ -1,10 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
+import { CreateUserCommand } from './create-user.command';
 import { CreateUserRequest } from './create-user.request.dto';
 
 @Controller('users')
 export class CreateUserController {
+  constructor(private readonly commandBus: CommandBus) {}
+
   @Post()
   async create(@Body() body: CreateUserRequest): Promise<void> {
-    console.log(body);
+    const command = new CreateUserCommand(body);
+
+    const result = await this.commandBus.execute(command);
+
+    return result;
   }
 }
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CreateUserController } from './commands/create-user/create-user.controller';
+import { CreateUserCommandHandler } from './commands/create-user/create-user.command-handler';
 import { UserOrmEntity } from './database/user.orm-entity';
 
+const commandHandlers = [CreateUserCommandHandler];
+
 @Module({
-  imports: [TypeOrmModule.forFeature([UserOrmEntity])],
+  imports: [TypeOrmModule.forFeature([UserOrmEntity]), CqrsModule],
   controllers: [CreateUserController],
+  providers: [...commandHandlers],
 })
 export class UserModule {}
