1f9920bead65523b218bfe827d8598b604c9d198
Seunghyuk <seunghyukoh0@gmail.com>
2022-04-23 17:54:53 +0900

    Add interface adapter's interface

--- src/modules/user/commands/create-user/create-user.request.dto.ts
+++ src/modules/user/commands/create-user/create-user.request.dto.ts
@@ -1,6 +1,7 @@
 import { IsString, MaxLength, MinLength } from 'class-validator';
+import { CreateUser } from 'src/interface-adapters/interfaces/user/create-user.interface';
 
-export class CreateUserRequest {
+export class CreateUserRequest implements CreateUser {
   @IsString()
   @MaxLength(200)
   @MinLength(5)
