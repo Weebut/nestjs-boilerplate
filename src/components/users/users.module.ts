915c7271d165e03ef020b1da7832ea12aba120cb
Seunghyuk <seunghyukoh0@gmail.com>
2022-04-23 18:07:27 +0900

    Use users controller

--- src/components/users/users.module.ts
+++ src/components/users/users.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CreateUserController } from './commands/create-user/create-user.controller';
 import { CreateUserCommandHandler } from './commands/create-user/create-user.command-handler';
 import { UserOrmEntity } from './database/user.orm-entity';
+import { UsersController } from './users.controller';
 
 const commandHandlers = [CreateUserCommandHandler];
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserOrmEntity]), CqrsModule],
-  controllers: [CreateUserController],
+  controllers: [UsersController],
   providers: [...commandHandlers],
 })
 export class UsersModule {}
