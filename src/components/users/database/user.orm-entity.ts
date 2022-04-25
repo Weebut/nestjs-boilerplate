fd54a152ff17dcd9f0f32f77bfd420f6dc62ec12
Seunghyuk <seunghyukoh0@gmail.com>
2022-04-25 12:50:32 +0900

    Implement user orm entity

--- src/components/users/database/user.orm-entity.ts
+++ src/components/users/database/user.orm-entity.ts
@@ -1,5 +1,25 @@
 import { BaseTypeormEntity } from '@libs/structure/infrastructure/database/base-classes/base-typeorm-entity';
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
+import { UserRoles } from '../domain/entities/user.type';
 
 @Entity('user')
-export class UserOrmEntity extends BaseTypeormEntity {}
+export class UserOrmEntity extends BaseTypeormEntity {
+  constructor(props?: UserOrmEntity) {
+    super(props);
+  }
+
+  @Column({ type: 'varchar', nullable: false, length: 320, unique: true })
+  email: string;
+
+  @Column({ type: 'varchar', nullable: false, length: 32, unique: true })
+  nickname: string;
+
+  @Column({ type: 'varchar', nullable: false, length: 32 })
+  familyName: string;
+
+  @Column({ type: 'varchar', nullable: false, length: 32 })
+  givenName: string;
+
+  @Column({ type: 'enum', enum: UserRoles, default: UserRoles.GUEST })
+  role: UserRoles;
+}
