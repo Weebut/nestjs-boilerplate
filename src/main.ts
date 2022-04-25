6b249096bbe8650efee1e178832390b185caa699
Seunghyuk <seunghyukoh0@gmail.com>
2022-04-25 15:06:22 +0900

    Add common things

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { config } from 'dotenv';
 import { AppModule } from 'src/app.module';
+import { AllExceptionsFilter } from './common/filters/exceptions.filter';
+import { LoggingInterceptor } from './common/interceptors/logger.interceptors';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import { LoggerService } from './common/logger/logger.service';
 
 config();
 
@@ -14,6 +18,15 @@ async function bootstrap() {
   app.enableVersioning({ type: VersioningType.URI, defaultVersion: v1 });
 
   app.useGlobalPipes(new ValidationPipe());
+
+  // Filters
+  app.useGlobalFilters(new AllExceptionsFilter(new LoggerService()));
+
+  // Interceptors
+  app.useGlobalInterceptors(
+    new LoggingInterceptor(new LoggerService()),
+    new ResponseInterceptor(),
+  );
 
   await app.listen(port, () => console.log('Listening on port', port));
 }
