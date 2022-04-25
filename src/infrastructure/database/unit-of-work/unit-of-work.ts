e7847ef07b71fb7957820d3bd02d5b9a5fe4cafe
Seunghyuk <seunghyukoh0@gmail.com>
2022-04-25 14:37:27 +0900

    Add a method to unit of work class; get users repository

--- src/infrastructure/database/unit-of-work/unit-of-work.ts
+++ src/infrastructure/database/unit-of-work/unit-of-work.ts
@@ -1,7 +1,15 @@
+import { UserOrmEntity } from '@components/users/database/user.orm-entity';
+import { UsersRepository } from '@components/users/database/users.repository';
 import { TypeormUnitOfWork } from '@libs/structure/infrastructure/database/unit-of-work/typeorm.unit-of-work';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UnitOfWork extends TypeormUnitOfWork {
   // Add new repositories below to use this generic UnitOfWork
+
+  getUsersRepository(correlationId: string): UsersRepository {
+    return new UsersRepository(
+      this.getOrmRepository(UserOrmEntity, correlationId),
+    ).setCorrelationId(correlationId);
+  }
 }
