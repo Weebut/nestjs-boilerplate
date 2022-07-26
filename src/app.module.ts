66594112c22b50b255cf42cb04f683e730d5247f
ohshyuk5 <seunghyukoh0@gmail.com>
2022-07-26 13:10:09 +0900

    Add event emitter config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { ComponentsModule } from '@components/components.module';
 import { EnvironmentConfigModule } from '@infrastructure/configs/environment/environment.module';
+import { EventEmitterConfigModule } from '@infrastructure/configs/event-emitter/event-emitter.module';
 import { TypeormConfigModule } from '@infrastructure/configs/typeorm/typeorm.module';
 import { UnitOfWorkModule } from '@infrastructure/database/unit-of-work/unit-of-work.module';
 import { Module } from '@nestjs/common';
@@ -9,6 +10,7 @@ import { AppService } from 'src/app.service';
 @Module({
   imports: [
     EnvironmentConfigModule,
+    EventEmitterConfigModule,
     TypeormConfigModule,
     UnitOfWorkModule,
     ComponentsModule,
